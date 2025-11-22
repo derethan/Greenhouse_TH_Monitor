4c28ded72dd10998a443282c81d7b1666c56b243
Andrew Patterson <apatterson93@gmail.com>
2025-11-22 10:25:10 -0330

    Moved firmware from GG Core repo

---
+++ include/secrets.h
@@ -0,0 +1,11 @@
+// arduino_secrets.h header file
+
+#define SECRET_SSID "BATECH_Camera"
+#define SECRET_PASS "Norton66"
+
+#define AP_SSID "GG-TH-"
+#define AP_PASS "Norton66"
+
+#define SECRET_API_SERVER "192.168.0.164"
+
+#define SECRET_PORT 3420
